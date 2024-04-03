6b2b4212d440c6cdb68e317c4ebd5f3c667dcdee
Abdul Rehman <ar618211@gmail.com>
2024-04-03 13:32:29 +0500

    group anagram done but could be bette

---
+++ internal/groupanagrams/groupanagrams.go
@@ -0,0 +1,26 @@
+package groupanagrams
+
+type counter [26]int
+
+func GroupAnagrams(strs []string) [][]string {
+	var a rune = 'a'
+
+	mapp := make(map[counter]uint32)
+	var out [][]string
+
+	for _, str := range strs {
+		key := counter{}
+		for _, ch := range str {
+			key[ch - a]++
+		}
+		if _, has := mapp[key]; has {
+			out[mapp[key]] = append(out[mapp[key]], str)
+		} else {
+			mapp[key] = uint32(len(out))
+			out = append(out, []string{str})
+		}
+	}
+
+	return out
+}
+
---
+++ internal/groupanagrams/groupanagrams_test.go
@@ -0,0 +1,20 @@
+package groupanagrams_test
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// inputs := [3][]string{
+	// 	{"eat","tea","tan","ate","nat","bat"},
+	// 	{""},
+	// 	{"a"},
+	// }
+	// outputs := [3][][]string{
+	// 	{{"bat"},{"nat","tan"},{"ate","eat","tea"}},
+	// 	{{""}},
+	// 	{{"a"}},
+	// }
+
+}
+

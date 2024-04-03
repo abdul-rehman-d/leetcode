1cd13d3a1d0d0cfdd493a6875abb615906a38e92
Abdul Rehman <ar618211@gmail.com>
2024-04-03 23:24:47 +0500

    top k frequent done but could be bette

---
+++ internal/topkfrequent/topkfrequent.go
@@ -0,0 +1,30 @@
+package topkfrequent
+
+func TopKFrequent(nums []int, k int) []int {
+
+	res := make([][]int, len(nums))
+	var out []int
+
+	freq := make(map[int]int, len(nums))
+	for _, num := range nums {
+		freq[num]++
+	}
+	for num, count := range freq {
+		res[count-1] = append(res[count-1], num)
+	}
+
+	var count int
+	for i := len(res)-1; i>=0; i-- {
+		if len(res[i]) == 0 {
+			continue
+		}
+		out = append(out, res[i]...)
+		count += len(res[i])
+		if count == k {
+			break
+		}
+	}
+
+	return out
+}
+
---
+++ internal/topkfrequent/topkfrequent_test.go
@@ -0,0 +1,53 @@
+package topkfrequent_test
+
+import (
+	"testing"
+
+	"github.com/abdul-rehman-d/leetcode/internal/topkfrequent"
+)
+
+func areArrsSame(arr1, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	mapp := make(map[int]int, len(arr1))
+	for _, n := range arr1 {
+		mapp[n]++
+	}
+	for _, n := range arr2 {
+		if mapp[n] == 0 {
+			return false
+		}
+		mapp[n]--
+	}
+	for _, val := range mapp {
+		if val != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestMain(t *testing.T) {
+	inputs := [2]struct{
+		Arr []int;
+		K int;
+	}{
+		{Arr: []int{1,1,1,2,2,3}, K: 2},
+		{Arr: []int{1}, K: 1},
+	}
+	outputs := [2][]int{
+		{1,2},
+		{1},
+	}
+
+	for idx, input := range inputs {
+		result := topkfrequent.TopKFrequent(input.Arr, input.K)
+		if !areArrsSame(result, outputs[idx]) {
+			t.Logf("\nInput: %v\nExpected: %v\nActual: %v", input, outputs[idx], result)
+			t.Fail()
+		}
+	}
+}
+

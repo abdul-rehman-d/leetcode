5eb945807d57e44e98c5643945f3cd796ebfc28d
Abdul Rehman <ar618211@gmail.com>
2024-04-03 13:03:12 +0500

    two sum done

---
+++ internal/twosum/twosum.go
@@ -0,0 +1,14 @@
+package twosum
+
+func TwoSum(nums []int, target int) []int {
+	mapp := make(map[int]int, len(nums))
+	for idx, num := range nums {
+		val, has := mapp[num]
+		if has {
+			return []int{val, idx}
+		}
+		mapp[target-num] = idx
+	}
+	return []int{}
+}
+
---
+++ internal/twosum/twosum_test.go
@@ -0,0 +1,55 @@
+package twosum_test
+
+import (
+	"testing"
+
+	"github.com/abdul-rehman-d/leetcode/internal/twosum"
+)
+
+func areArrsSame(arr1, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	mapp := make(map[int]int, len(arr1))
+	for _, n := range arr1 {
+		mapp[n]++
+	}
+	for _, n := range arr2 {
+		if mapp[n] == 0 {
+			return false
+		}
+		mapp[n]--
+	}
+	for _, val := range mapp {
+		if val != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestMain(t *testing.T) {
+	inputs := [3]struct{
+		Arr []int;
+		Target int;
+	}{
+		{Arr: []int{2,7,11,15}, Target: 9},
+		{Arr: []int{3,2,4}, Target: 6},
+		{Arr: []int{3,3}, Target: 6},
+	}
+	outputs := [3][]int{
+		{0,1},
+		{1,2},
+		{0,1},
+	}
+
+	for idx, input := range inputs {
+		result := twosum.TwoSum(input.Arr, input.Target)
+		if !areArrsSame(result, outputs[idx]) {
+			t.Logf("\nInput: %v\nExpected: %v\nActual: %v", input, outputs[idx], result)
+			t.Fail()
+		}
+	}
+}
+

e1f8fda4dfa1667243a3f16679ebd538f958e17f
Abdul Rehman <ar618211@gmail.com>
2024-04-05 03:04:38 +0500

    is sudoku valid made better

--- internal/isvalidsudoku/isvalidsudoku.go
+++ internal/isvalidsudoku/isvalidsudoku.go
@@ -1,35 +1,36 @@
 package isvalidsudoku
 
 func IsValidSudoku(board [][]byte) bool {
-	rowfreq := make([]map[byte]bool, len(board))
-	colfreq := make([]map[byte]bool, len(board[0]))
-	var freq3x3 [3][3]map[byte]bool
-	for i := 0; i < 9; i++ {
-		rowfreq[i] = make(map[byte]bool, 9)
-		colfreq[i] = make(map[byte]bool, 9)
-	}
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			freq3x3[i][j] = make(map[byte]bool, 9)
-		}
-	}
+	var rowfreq [9][9]bool
+	var colfreq [9][9]bool
+	var freq3x3 [9][9]bool
+
 	for y, row := range board {
 		for x, cell := range row {
 			if cell == '.' {
 				continue
 			}
+
+			cell = cell - '0' - 1
+
+			// row check
 			if rowfreq[y][cell] {
 				return false
 			}
+
+			// col check
 			rowfreq[y][cell] = true
 			if colfreq[x][cell] {
 				return false
 			}
 			colfreq[x][cell] = true
-			if freq3x3[y/3][x/3][cell] {
+
+			// block check
+			key := (y/3 * 3) + x/3
+			if freq3x3[key][cell] {
 				return false
 			}
-			freq3x3[y/3][x/3][cell] = true
+			freq3x3[key][cell] = true
 		}
 	}
 	return true

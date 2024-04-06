89e53273fd07fe544103d092a73a2801709376ab
Abdul Rehman <ar618211@gmail.com>
2024-04-07 04:41:39 +0500

    container w most water done

---
+++ internal/container_with_most_water/container_with_most_water.go
@@ -0,0 +1,24 @@
+package container_with_most_water
+
+func MaxArea(height []int) int {
+s := 0
+	e := len(height)-1
+	maxArea := 0
+
+	for s < e {
+		w := e - s
+		h := min(height[s], height[e])
+		
+		maxArea = max(w*h, maxArea)
+
+		if height[s] > height[e] {
+			e--
+		} else {
+			s++
+		}
+
+	}
+
+    return maxArea
+}
+
---
+++ internal/container_with_most_water/container_with_most_water_test.go
@@ -0,0 +1,24 @@
+package container_with_most_water_test
+
+import (
+	"testing"
+
+	"github.com/abdul-rehman-d/leetcode/internal/container_with_most_water"
+)
+
+func TestMain(t *testing.T) {
+	inputs := [2][]int{
+        {1,8,6,2,5,4,8,3,7},
+        {1,1},
+	}
+	outputs := [2]int{49,1}
+
+	for idx, input := range inputs {
+		result := container_with_most_water.MaxArea(input)
+		if result != outputs[idx] {
+			t.Logf("\nInput: %v\nExpected: %v\nActual: %v", input, outputs[idx], result)
+			t.Fail()
+		}
+	}
+}
+

912c1a807196984d72f0f5b90a3e5f5073942007
Abdul Rehman <ar618211@gmail.com>
2025-10-22 15:55:37 +0500

    updated longest repeating character replacement tests

--- internal/longest_repeating_character_replacement/longest_repeating_character_replacement_test.go
+++ internal/longest_repeating_character_replacement/longest_repeating_character_replacement_test.go
@@ -7,15 +7,16 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	inputs := [3]struct {
+	inputs := []struct {
 		s string
 		k int
 	}{
 		{s: "ABAB", k: 2},
 		{s: "AABABBA", k: 1},
 		{s: "AAAA", k: 0},
+		{s: "ABBB", k: 2},
 	}
-	outputs := [3]int{4, 4, 4}
+	outputs := []int{4, 4, 4, 4}
 
 	for idx, input := range inputs {
 		result := longest_repeating_character_replacement.CharacterReplacement(input.s, input.k)
